7f6703e0856e2c72d61b1c030912d7daa60ef6fc
Dilshad <dilshad_fw11_177@masai.school>
2022-03-24 04:23:20 +0530

    Updates

--- src/redux/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TODO = "ADD_TODO";
-export const DEL_TODO = "DEL_TODO";
-export const EDIT_TODO = "EDIT_TODO";
-export const REMOVE_ALL = "REMOVE_ALL"
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import {todoReducer} from './redux/reducer';
-import { createStore } from 'redux';
-
-export const store = createStore(todoReducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
-store.subscribe(()=>{
-    console.log("todo changed")
-})
